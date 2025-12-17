096bbf07a19ea02130cb400d8e101906f13bf129
EstiaanJ <xero416@gmail.com>
2025-12-17 13:04:42 +0900

    Added Tests root

---
+++ tests/xyz/ejvr/physics/VectorDoubleTest.java
@@ -0,0 +1,94 @@
+package xyz.ejvr.physics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorDoubleTest {
+
+    @org.junit.jupiter.api.Test
+    void diffX() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void diffY() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void radiusSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void radius() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void angle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void xFloat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void yFloat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void rotateAboutOrigin() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void distanceBetween() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dotProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void crossProduct2D() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void negate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sub() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void tangent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void scale() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void normalize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void rotatePoint() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void x() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void y() {
+    }
+}
